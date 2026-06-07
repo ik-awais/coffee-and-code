9afca83ae37ea0682f8b3fdda487f686a44c73b2
ik-awais <mawaisqq@gmail.com>
2026-06-07 12:26:23 +0500

    chore: testing git sync

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let myName1 = 'Shahroz Shahid';
 let myName2 = 'All Rounder';
 let myName3 = 'Patrisha';
 
-
 document.getElementById('h1').textContent= `My name is ${myName}`;
 document.getElementById('p').textContent = `and I m only ${myName}`;
 document.getElementById('h2').textContent= `My name is ${myName1}`;
